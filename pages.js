01d05d1d491d6cd6de11ee6de01cd5ac27dd3a4a
ela_lisondra <emrlisondra@gmail.com>
2021-07-31 14:18:28 +0100

    started coding js function for next and previous buttons

---
+++ pages.js
@@ -0,0 +1,38 @@
+let pageNumber = 0
+
+const nextTag = document.querySelector('footer img.next')
+const previousTag = document.querySelector('footer img.prev')
+const outputTag = document.querySelector('h2')
+
+const next = function () {
+    pageNumber = pageNumber + 1
+
+    if (pageNumber > 10){
+        pageNumber = 1
+    }
+
+    updateSection()
+}
+
+const previous = function () {
+    pageNumber = pageNumber - 1
+
+    if (pageNumber < 1) {
+        pageNumber = 10
+    }
+
+    updateSection()
+}
+
+const updateSection = function () {
+    outputTag.innerHTML = pageNumber
+}
+
+nextTag.addEventListener("click", function() {
+    next()
+   
+})
+
+previousTag.addEventListener("click", function () {
+    previous()
+})
