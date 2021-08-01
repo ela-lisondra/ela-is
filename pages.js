a520677782e27e3698e94536d66ad44110e89d73
ela_lisondra <emrlisondra@gmail.com>
2021-08-01 02:56:12 +0100

    added data

--- pages.js
+++ pages.js
@@ -1,14 +1,27 @@
 let pageNumber = 0
 
+const pages = [
+    { copy: "a massive foodie her ex used to call her 'Kirby' from Pokemon as a pet name", background: "#edc7a9", 
+    circle: "#3e78ed" },
+    { copy: "in love with life and curious about many things in this world and beyond", background: "#a1fffe",
+    circle: "#e34a47" },
+    { copy: "a yoga enthusiast especially hot yoga in winter. She's also a certified Children's Yoga Educator", background: "#d3c7f3",
+    circle: "#f7fe00" },
+    { copy: "letting you <a href='pdf.pdf'>download her CV</a>", background: "#faffb8", circle: "#b472e6" }
+]
+
 const nextTag = document.querySelector('footer img.next')
 const previousTag = document.querySelector('footer img.prev')
 const outputTag = document.querySelector('h2')
+const circleTag = document.querySelector('section div.circle')
+const bodyTag = document.querySelector('body')
+
 
 const next = function () {
     pageNumber = pageNumber + 1
 
-    if (pageNumber > 10){
-        pageNumber = 1
+    if (pageNumber > pages.length - 1){
+        pageNumber = 0
     }
 
     updateSection()
@@ -17,15 +30,15 @@ const next = function () {
 const previous = function () {
     pageNumber = pageNumber - 1
 
-    if (pageNumber < 1) {
-        pageNumber = 10
+    if (pageNumber < 0) {
+        pageNumber = pages.length - 1
     }
 
     updateSection()
 }
 
 const updateSection = function () {
-    outputTag.innerHTML = pageNumber
+    outputTag.innerHTML = pages[pageNumber].copy
 }
 
 nextTag.addEventListener("click", function() {
